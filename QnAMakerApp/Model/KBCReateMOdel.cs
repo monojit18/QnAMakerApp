df1bef052d7b26edac5a023d0ba4d084d34bc807
monojit18 <datta.monojit@gmail.com>
2020-02-23 00:59:42 +0530

    Initial commit

---
+++ QnAMakerApp/Model/KBCReateMOdel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
+namespace QnAMakerApp.Model
+{
+    public class KBCReateModel
+    {
+
+        [JsonProperty("name")]
+        public string Name { get; set; }             
+
+        [JsonProperty("questions")]
+        public List<QNAModel> QNAList { get; set; }
+        
+    }
+}
---
+++ QnAMakerApp/Model/QNAModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace QnAMakerApp.Model
+{
+    public class QNAModel
+    {
+
+        [JsonProperty("id")]
+        public int id { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("answer")]
+        public string Answer { get; set; }
+
+        [JsonProperty("questions")]
+        public List<string> Questions { get; set; }
+
+    }
+}
